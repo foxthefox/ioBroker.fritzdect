15877e12cc3df2d9dbcd131d2e708afbb93212fe
foxthefox <github@wysiwis.net>
2016-01-29 22:59:11 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,119 @@
+/* jshint -W097 */// jshint strict:false
+/*jslint node: true */
+
+
+"use strict";
+
+// you have to require the utils module and call adapter function
+var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
+
+// you have to call the adapter function and pass a options object
+// name has to be set and has to be equal to adapters folder name and main file name excluding extension
+// adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.template.0
+var adapter = utils.adapter('jeelink');
+
+// is called when adapter shuts down - callback has to be called under any circumstances!
+adapter.on('unload', function (callback) {
+    try {
+        adapter.log.info('cleaned everything up...');
+        callback();
+    } catch (e) {
+        callback();
+    }
+});
+
+// is called if a subscribed object changes
+adapter.on('objectChange', function (id, obj) {
+    // Warning, obj can be null if it was deleted
+    adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
+});
+
+// is called if a subscribed state changes
+adapter.on('stateChange', function (id, state) {
+    // Warning, state can be null if it was deleted
+    adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
+
+    // you can use the ack flag to detect if it is status (true) or command (false)
+    if (state && !state.ack) {
+        adapter.log.info('ack is not set!');
+    }
+});
+
+// Some message was sent to adapter instance over message box. Used by email, pushover, text2speech, ...
+adapter.on('message', function (obj) {
+    if (typeof obj == 'object' && obj.message) {
+        if (obj.command == 'send') {
+            // e.g. send email or pushover or whatever
+            console.log('send command');
+
+            // Send response in callback if required
+            if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
+        }
+    }
+});
+
+// is called when databases are connected and adapter received configuration.
+// start here!
+adapter.on('ready', function () {
+    main();
+});
+
+function main() {
+
+    // The adapters config (in the instance object everything under the attribute "native") is accessible via
+    // adapter.config:
+    adapter.log.info('config test1: ' + adapter.config.test1);
+    adapter.log.info('config test1: ' + adapter.config.test2);
+
+    var options = {
+        bridge:     adapter.config.milight-ip       || '192.168.178.33',
+        port:       adapter.config.milight-port     || 8899
+    };
+
+    /**
+     *
+     *      For every state in the system there has to be also an object of type state
+     *
+     *      Here a simple template for a boolean variable named "testVariable"
+     *
+     *      Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
+     *
+     */
+
+
+
+    // in this template all states changes inside the adapters namespace are subscribed
+    adapter.subscribeStates('*');
+
+
+    /**
+     *   setState examples
+     *
+     *   you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
+     *
+     */
+
+    // the variable testVariable is set to true as command (ack=false)
+    adapter.setState('testVariable', true);
+
+    // same thing, but the value is flagged "ack"
+    // ack should be always set to true if the value is received from or acknowledged from the target system
+    adapter.setState('testVariable', {val: true, ack: true});
+
+    // same thing, but the state is deleted after 30s (getState will return null afterwards)
+    adapter.setState('testVariable', {val: true, ack: true, expire: 30});
+
+
+
+    // examples for the checkPassword/checkGroup functions
+    adapter.checkPassword('admin', 'iobroker', function (res) {
+        console.log('check user admin pw ioboker: ' + res);
+    });
+
+    adapter.checkGroup('admin', 'admin', function (res) {
+        console.log('check group user admin group admin: ' + res);
+    });
+
+
+
+}

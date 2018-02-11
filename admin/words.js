687c045638b909e6793ebd8c287a07cc04be39ec
foxthefox <github@wysiwis.net>
2018-02-11 14:13:10 +0100

    Update words.js

--- admin/words.js
+++ admin/words.js
@@ -6,5 +6,49 @@ systemDictionary = {
     	"Fritzbox Password": {"en": "Fritzbox Password", "de": "Fritzbox Password", "ru": "Пароль Fritzbox",        "pt": "Senha Fritzbox",         "nl": "Fritzbox wachtwoord",    "fr": "Mot de passe Fritzbox",  "it": "Fritzbox Password",      "es": "Contrase�a de Fritzbox", "pl": "Haslo Fritzboksa"},
 	"Fritzbox Polling Interval": {"en": "Fritzbox Polling Interval", "de": "Fritzbox Polling Interval", "ru": "Интервал опроса Fritzbox",                       "pt": "Fritzbox Polling Interval",                      "nl": "Fritzbox Polling - interval",                      "fr": "Fritzbox Intervalle d'interrogation",            "it": "Intervallo di polling Fritzbox",                 "es": "Intervalo de interrogaci�n de Fritzbox",         "pl": "Fritzbox Polling Interval"},
 	"Fritzbox User": {"en": "Fritzbox User", "de": "Fritzbox User", "ru": "Пользователь Fritzbox",  "pt": "Usu�rio de Fritzbox",    "nl": "Fritzbox - gebruiker",     "fr": "Utilisateur de Fritzbox",                        "it": "Utente Fritzbox",        "es": "Usuario de Fritzbox",    "pl": "Uzytkownik Fritzboksa"},
-	"fritzdect adapter settings": {"en": "fritzdect adapter setup", "de": "fritzdect Adapter Einstellung", "ru": "Настройка адаптера fritzdect",                   "pt": "configura��o do adaptador fritzdect",            "nl": "fritzdect adapter instellen",                    "fr": "configuration de l'adaptateur fritzdect",        "it": "impostazione dell'adattatore fritzdect",         "es": "configuraci�n del adaptador de Fritzdect",       "pl": "Konfiguracja adaptera fritzdect"}
+	"fritzdect adapter settings": {"en": "fritzdect adapter setup", "de": "fritzdect Adapter Einstellung", "ru": "Настройка адаптера fritzdect",                   "pt": "configura��o do adaptador fritzdect",            "nl": "fritzdect adapter instellen",                    "fr": "configuration de l'adaptateur fritzdect",        "it": "impostazione dell'adattatore fritzdect",         "es": "configuraci�n del adaptador de Fritzdect",       "pl": "Konfiguracja adaptera fritzdect"},
+	"FritzBox Adapter Debugging": {
+		  "en": "FritzBox Adapter Debugging",
+		  "de": "FritzBox Adapter Debuggen",
+		  "ru": "Отладка адаптера FritzBox",
+		  "pt": "Depuração do adaptador FritzBox",
+		  "nl": "FritzBox Adapter Debugging",
+		  "fr": "Débogage de l'adaptateur FritzBox",
+		  "it": "Debug della scheda FritzBox",
+		  "es": "Depuración del adaptador FritzBox",
+		  "pl": "Debugowanie FritzBox Adapter"
+		}
+		"My Devices": {
+		  "en": "My Devices",
+		  "de": "Meine Geräte",
+		  "ru": "Мои устройства",
+		  "pt": "Meus dispositivos",
+		  "nl": "Mijn apparaten",
+		  "fr": "Mes appareils",
+		  "it": "I miei dispositivi",
+		  "es": "Mis dispositivos",
+		  "pl": "Moje urządzenia"
+		},
+		"My Groups": {
+		  "en": "My Groups",
+		  "de": "Meine Gruppen",
+		  "ru": "Мои группы",
+		  "pt": "Meus grupos",
+		  "nl": "Mijn groepen",
+		  "fr": "Mes groupes",
+		  "it": "I miei gruppi",
+		  "es": "Mis grupos",
+		  "pl": "Moje grupy"
+		},
+		"include GuestWLAN in adapter": {
+		  "en": "include GuestWLAN in adapter",
+		  "de": "Schließen Sie GuestWLAN in den Adapter ein",
+		  "ru": "включить гостевой WLAN в адаптер",
+		  "pt": "incluir GuestWLAN no adaptador",
+		  "nl": "inclusief GuestWLAN in adapter",
+		  "fr": "inclure GuestWLAN dans l'adaptateur",
+		  "it": "includere GuestWLAN nell'adattatore",
+		  "es": "incluir GuestWLAN en el adaptador",
+		  "pl": "dołączyć GuestWLAN do adaptera"
+		}
 };

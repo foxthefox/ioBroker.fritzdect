fef0ee357a85f17278a0e3b2f761c27fecc84ee2
foxthefox <github@wysiwis.net>
2018-06-11 07:25:25 +0200

    Update words.js

--- admin/words.js
+++ admin/words.js
@@ -4,7 +4,17 @@
 systemDictionary = {
 	"Fritzbox IP": {"en": "Fritzbox IP", "de": "Fritzbox IP", "ru": "Fritzbox IP","pt": "Fritzbox IP", "nl": "Fritzbox IP", "fr": "Fritzbox IP", "it": "Fritzbox IP", "es": "Fritzbox IP", "pl": "Fritzbox IP"},
     	"Fritzbox Password": {"en": "Fritzbox Password", "de": "Fritzbox Password", "ru": "Пароль Fritzbox",        "pt": "Senha Fritzbox",         "nl": "Fritzbox wachtwoord",    "fr": "Mot de passe Fritzbox",  "it": "Fritzbox Password",      "es": "Contrase�a de Fritzbox", "pl": "Haslo Fritzboksa"},
-	"Fritzbox Polling Interval": {"en": "Fritzbox Polling Interval", "de": "Fritzbox Polling Interval", "ru": "Интервал опроса Fritzbox",                       "pt": "Fritzbox Polling Interval",                      "nl": "Fritzbox Polling - interval",                      "fr": "Fritzbox Intervalle d'interrogation",            "it": "Intervallo di polling Fritzbox",                 "es": "Intervalo de interrogaci�n de Fritzbox",         "pl": "Fritzbox Polling Interval"},
+	"Fritzbox Polling Interval (sek.)": {
+		    "en": "Fritzbox Polling Interval (sek.)",
+		    "de": "Fritzbox Abrufintervall (sek.)",
+		    "ru": "Интервал опроса Fritzbox (сек.)",
+		    "pt": "Intervalo de Polling do Fritzbox (sek.)",
+		    "nl": "Fritzbox Polling Interval (sek.)",
+		    "fr": "Fritzbox Intervalle d'interrogation (sek.)",
+		    "it": "Fritzbox Polling Interval (sek.)",
+		    "es": "Intervalo de sondeo de Fritzbox (sek)",
+		    "pl": "Fritzbox Polling Interval (sek.)"
+		  }, 
 	"Fritzbox User": {"en": "Fritzbox User", "de": "Fritzbox User", "ru": "Пользователь Fritzbox",  "pt": "Usu�rio de Fritzbox",    "nl": "Fritzbox - gebruiker",     "fr": "Utilisateur de Fritzbox",                        "it": "Utente Fritzbox",        "es": "Usuario de Fritzbox",    "pl": "Uzytkownik Fritzboksa"},
 	"fritzdect adapter settings": {"en": "fritzdect adapter setup", "de": "fritzdect Adapter Einstellung", "ru": "Настройка адаптера fritzdect",                   "pt": "configura��o do adaptador fritzdect",            "nl": "fritzdect adapter instellen",                    "fr": "configuration de l'adaptateur fritzdect",        "it": "impostazione dell'adattatore fritzdect",         "es": "configuraci�n del adaptador de Fritzdect",       "pl": "Konfiguracja adaptera fritzdect"},
 	"FritzBox Adapter Debugging": {

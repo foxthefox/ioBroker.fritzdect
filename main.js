89f86f8802d9c01f511b0ba0d91839833d2d7369
foxthefox <github@wysiwis.net>
2020-03-21 13:42:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -309,7 +309,8 @@ function startAdapter(options) {
                         }
                     }
                     if (dp == 'level') {
-                            fritz.setLevel(id, state.val).then(function (sid) {
+                            fritz.setLevel(id, parseInt(state.val/100*255) ).then(function (sid) { //level is in 0...255
+                                
                                 adapter.log.debug('Set lamp level' + id + ' to '+ state.val);
                                 adapter.setState('DECT500_'+ id +'.level', {val: state.val, ack: true}); //iobroker State-Bedienung wird nochmal als Status geschrieben, da API-Aufruf erfolgreich
                             })
